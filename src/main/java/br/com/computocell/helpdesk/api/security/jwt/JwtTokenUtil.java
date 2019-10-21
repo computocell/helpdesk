8bd67e785a4cfd20fd59c29e64743eeb82ea858f
Jefferson <jefferson.admsistemas@gmail.com>
2019-10-21 00:43:17 -0300

    Criado a classe JwtTokenUtil

--- src/main/java/br/com/computocell/helpdesk/api/security/jwt/JwtTokenUtil.java
+++ src/main/java/br/com/computocell/helpdesk/api/security/jwt/JwtTokenUtil.java
@@ -13,7 +13,7 @@ import java.util.Map;
 
 
 public class JwtTokenUtil implements Serializable {
-    private static final long serialVersionUID=1L;
+    private static final long serialVersionUID = 1L;
 
     static final String CLAIM_KEY_USERNAME  = "sub";
     static final String CLAIM_KEY_CREATED   = "created";
