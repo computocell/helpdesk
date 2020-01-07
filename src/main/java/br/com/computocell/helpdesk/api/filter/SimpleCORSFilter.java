d3bd3119a5849677abe9a5f85aac5bab0f7f26ec
Jefferson <jefferson.admsistemas@gmail.com>
2020-01-07 00:34:19 -0300

    Ajuste headers cors

--- src/main/java/br/com/computocell/helpdesk/api/filter/SimpleCORSFilter.java
+++ src/main/java/br/com/computocell/helpdesk/api/filter/SimpleCORSFilter.java
@@ -37,8 +37,7 @@ public class SimpleCORSFilter implements Filter {
 		response.setHeader("Access-Control-Allow-Origin", "*");
 		response.setHeader("Access-Control-Allow-Methods", "POST, GET, OPTIONS, DELETE, PUT");
 		response.setHeader("Access-Control-Max-Age", "3600");
-		response.setHeader("Access-Control-Allow-Header", "x-requested-with, authorization, Content-Type, Authorization, credential, X-XSRF-TOKEN");
-		
+		response.setHeader("Access-Control-Allow-Headers", "x-requested-with, authorization, Content-Type, Authorization, credential, X-XSRF-TOKEN");
 		if("OPTIONS".equalsIgnoreCase(request.getMethod())) {
 			response.setStatus(HttpServletResponse.SC_OK);
 		}else {
